4eb35837df3bc5875c5b82100d45581cbd5f481f
mroberts <molly.e.roberts@gmail.com>
2015-02-28 14:43:48 -0800

    ..

--- R/vizjson.R
+++ R/vizjson.R
@@ -81,6 +81,6 @@ vizjson <- function(mod, data, covariates, text, id=NULL, n=1000,
     close(fileConn2)
     setwd("..")
     print(paste("URL is at file://", getwd(),
-                "index.html, opening now.", sep="")
+                "index.html, opening now.", sep=""))
     BROWSE(paste("file://", getwd() "../index.html", sep=""))
 }
